b4d70911da579a62d629ad79102d80df00efbd53
(w) Iker Hernaez <ihernaez@users.noreply.github.com>
2026-07-11 12:41:49 +0200

    Add missing packages Javadoc

---
+++ orne-beans-javax/src/main/java/dev/orne/beans/rnd/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * {@code Orne Generators} based random value generators for testing purposes.
+ * 
+ * @since 0.6
+ * @see <a href="https://orne-dev.github.io/java-generators/">Orne Generators</a>
+ */
+package dev.orne.beans.rnd;
+
+/*-
+ * #%L
+ * Orne Beans
+ * %%
+ * Copyright (C) 2020 - 2025 Orne Developments
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
